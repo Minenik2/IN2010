4debec10122a2487223169cc74024a8d08aa221a
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2023-09-16 17:22:58 +0200

    progress

---
+++ oblig1/Lenketliste.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Lenketliste {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+        }
+    }
+
+    static void push_back() {
+
+    }
+
+    static void push_front() {
+
+    }
+
+    static void push_middle() {
+
+    }
+
+    static void get() {
+
+    }
+
+    
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
