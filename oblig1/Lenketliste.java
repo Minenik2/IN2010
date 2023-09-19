aa01dfa19d79174fdbab25b7647990bfcb81e5c1
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2023-09-19 13:11:44 +0200

    insertionsort

--- oblig1/Lenketliste.java
+++ oblig1/Lenketliste.java
@@ -13,11 +13,23 @@ class Lenketliste {
         Node(int d) {
             data = d;
         }
+
+        // return data er for sorteringsalgoritmene
+        int returnData() {
+            return data;
+        }
+
+        // set data er for sorteringsalgoritmene
+        void setData(int integer) {
+            data = integer;
+        }
     }
 
+    // denne toString metoden er ikke spurt av obligen men er her for å debugge
+    // lettere
     @Override
     public String toString() {
-        String returnString = "senpai: ";
+        String returnString = "Noder: ";
         Node currentNode = head;
         for (int i = 0; i < length; i++) {
             returnString += currentNode.data + ", ";
@@ -28,7 +40,7 @@ class Lenketliste {
 
     void push_back(String streng) {
         /// ##System.out.println("lengden er: " + length);
-        getReturn(length - 1 + "").next = new Node(Integer.parseInt(streng));
+        getReturn(length - 1).next = new Node(Integer.parseInt(streng));
         length++;
     }
 
@@ -38,14 +50,13 @@ class Lenketliste {
         Node newNode = new Node(Integer.parseInt(streng));
         newNode.next = head;
         head = newNode;
-        /// ##System.out.println("hihihihi " + head.data);
         length++;
     }
 
     void push_middle(String integer) {
         Node nodeA = new Node(Integer.parseInt(integer));
-        System.out.println("arraynumber: " + ((Math.floor((length + 1) / 2))) + " Total: " + (length + 1) + " ");
-        Node nodeEBehind = getReturn((int) (Math.floor((length + 1) / 2)) - 1 + "");
+        System.out.println("arraynumber: " + ((Math.floor((length + 1) / 2))) + " Total: " + (length + 1));
+        Node nodeEBehind = getReturn((int) (Math.floor((length + 1) / 2)) - 1);
 
         if (nodeEBehind == null) {
             head = nodeA; // hvis det er ingenting i lenketlisten fra før av og middle push blir kalt
@@ -59,7 +70,7 @@ class Lenketliste {
         length++;
     }
 
-    void get(String integer) {
+    void get(int integer) {
         // printer ut elemtet som spurt av obligen MEN JEG RESPECTFULLY OG PERSONALLY
         // hadde printet ut alt i main metoden
         System.out.println(getReturn(integer).data);
@@ -67,27 +78,43 @@ class Lenketliste {
 
     // lager denne metoden fordi jeg bruker den i andre metoder også, men har et
     // annet navn fordi den ikke printer ut men istenfor returnerer noden
-    Node getReturn(String integer) {
+    Node getReturn(int integer) {
         Node currentNode = head;
-        for (int i = 0; i < Integer.parseInt(integer); i++) {
+        for (int i = 0; i < integer; i++) {
             currentNode = currentNode.next;
         }
-        // enten er vi out of bounds of får en errror som betyr at brukeren vår på lese
+        // enten er vi out of bounds of får en errror som betyr at brukeren vår må lese
         // dokumentasjonen litt mer
         // eller så funker alt det skal og vi er på den noden som er i indexen brukeren
         // valgte
         return currentNode;
     }
 
+    Lenketliste insertionSort(Lenketliste list) {
+        for (int i = 1; i <= list.returnLength() - 1; i++) {
+            int j = i;
+            while (j > 0 && list.getReturn(j - 1).returnData() > list.getReturn(j).returnData()) {
+                Node left = list.getReturn(j - 1);
+                int leftData = left.returnData();
+                Node right = list.getReturn(j);
+                left.setData(right.returnData());
+                right.setData(leftData);
+                j--;
+            }
+        }
+        return list;
+    }
+
+    // returnerer lengen til array brukes for insertionsortmetoden
+    int returnLength() {
+        return length;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("E:\\UiO\\semester 7\\in2010\\IN2010\\oblig1\\input_100");
         Scanner scan = new Scanner(file);
 
-        // String node1 = "1";
         Lenketliste nodeList = new Lenketliste();
-        // nodeList.push_front(node1);
-
-        /// ##System.out.println("numberu uno");
 
         String msg = scan.next();
         int amount = Integer.parseInt(msg);
@@ -103,8 +130,13 @@ class Lenketliste {
             } else if (msg.equals("push_middle")) {
                 nodeList.push_middle(scan.next());
             } else {
-                nodeList.get(scan.next());
+                nodeList.get(Integer.parseInt(scan.next()));
             }
         }
+
+        // Sortering for insertionSort
+        System.out.println(nodeList.insertionSort(nodeList).toString());
+
+        scan.close();
     }
 }
